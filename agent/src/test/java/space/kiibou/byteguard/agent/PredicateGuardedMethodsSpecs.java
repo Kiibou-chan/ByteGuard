1909dd15cd9c77fa9defde055f933738d6ac9117
Svenja Kernig <svenja.kernig@gmx.de>
2023-09-13 14:37:32 +0200

    reactivate PredicateGuardedMethodsSpecs

--- agent/src/test/java/space/kiibou/byteguard/agent/PredicateGuardedMethodsSpecs.java
+++ agent/src/test/java/space/kiibou/byteguard/agent/PredicateGuardedMethodsSpecs.java
@@ -1,26 +1,21 @@
 package space.kiibou.byteguard.agent;
 
+import space.kiibou.byteguard.annotation.SpecFor;
 import space.kiibou.byteguard.specification.GuardSpec;
 import space.kiibou.byteguard.specification.method.MethodSpec;
 
-// @SpecFor("space.kiibou.byteguard.agent.PredicateGuardedMethods")
+@SpecFor("space.kiibou.byteguard.agent.PredicateGuardedMethods")
 class PredicateGuardedMethodsSpecs extends GuardSpec {
 
     public MethodSpec method() {
         return methodSpec(
-                requires(() -> {
-                    System.out.println("test1");
-                    return false;
-                })
+                requires(() -> false)
         );
     }
 
     public MethodSpec method(String str) {
         return methodSpec(
-                requires(() -> {
-                    System.out.println("test2");
-                    return str.startsWith("a");
-                })
+                requires(() -> str.startsWith("a"))
         );
     }
 
