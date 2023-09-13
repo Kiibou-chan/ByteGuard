86124410fe5d762778dcece35ed75afc86a79b11
Svenja Kernig <svenja.kernig@gmx.de>
2023-09-13 14:34:05 +0200

    add agent tests

---
+++ agent/src/test/kotlin/space/kiibou/jguard/agent/IteratorWrapperTest.kt
@@ -0,0 +1,45 @@
+package space.kiibou.jguard.agent
+
+import org.junit.jupiter.api.assertThrows
+import space.kiibou.byteguard.agent.IteratorWrapper
+import space.kiibou.byteguard.exception.GuardViolationException
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class IteratorWrapperTest {
+
+    @Test
+    fun testCreateIteratorWrapper() {
+        val iter = IteratorWrapper(listOf("a", "b", "c").iterator())
+
+        assertGuard(isSet = false, "canCallNext", iter)
+    }
+
+    @Test
+    fun testCanCallNextGuardFieldExists() {
+        val wrapperClass = IteratorWrapper::class.java
+
+        wrapperClass.getDeclaredField("canCallNext")
+    }
+
+    @Test
+    fun testNextWithoutHasNextThrowsException() {
+        val iter = IteratorWrapper(listOf("a", "b", "c").iterator())
+
+        assertThrows<GuardViolationException> { iter.next() }
+    }
+
+    @Test
+    fun testNextWithHasNext() {
+        val iter = IteratorWrapper(listOf("a", "b", "c").iterator())
+
+        iter.hasNext()
+
+        assertGuard(isSet = true, "canCallNext", iter)
+
+        assertEquals("a", iter.next())
+
+        assertGuard(isSet = false, "canCallNext", iter)
+    }
+
+}
